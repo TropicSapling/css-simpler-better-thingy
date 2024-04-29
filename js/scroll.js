8ef0e6ffbae28036ff3276fa2b02576fe512b738
TropicSapling <tropical.productions.scratch@gmail.com>
2024-04-30 00:36:18 +0200

    scroll.js

---
+++ js/scroll.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+	let scroll  = 0;
+	let block   = false;
+	let blocker = setTimeout(() => block = false, 0);
+
+	// Always scroll the entire screen height
+	document.querySelector("body").addEventListener("wheel", e => {
+		e.preventDefault();
+
+		if(!block) {
+			block = true;
+			clearTimeout(blocker);
+
+			scroll += e.deltaY < 0 ? -screen.height : screen.height;
+
+			scrollTo({top: scroll, behavior: "smooth"})
+		}
+
+		blocker = setTimeout(() => block = false, 500)
+	}, {passive: false})
+})
